685076857ff75af5d6f6ec47c3257fd64990de5b
Furqan Abid <thisismefurqan@gmail.com>
2023-07-12 12:29:29 +0500

    Create tic tac toe game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export default function Board()
+{
+  return(
+  <>
+  <div className="boardRow">
+    <Square />
+    <Square />
+    <Square />
+  
+  </div>
+  
+  <div className="boardRow">
+    <Square />
+    <Square />
+    <Square />
+  </div>
+  
+  <div className="boardRow">
+    <Square />
+    <Square />
+    <Square />
+  </div>
+  </>
   );
 }
 
-export default App;
+function Square()
+{
+  return <button className = "square">1</button>;
+}
