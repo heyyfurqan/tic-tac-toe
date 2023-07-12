0c03f386d63d39bc1c5c5041a3c591b19345f5d6
Furqan Abid <thisismefurqan@gmail.com>
2023-07-12 15:02:49 +0500

    Add props for passing value to each square

--- src/App.js
+++ src/App.js
@@ -3,28 +3,37 @@ export default function Board()
   return(
   <>
   <div className="boardRow">
-    <Square />
-    <Square />
-    <Square />
+    <Square value="1" />
+    <Square value="2" />
+    <Square value="3" />
   
   </div>
   
   <div className="boardRow">
-    <Square />
-    <Square />
-    <Square />
+    <Square value="4" />
+    <Square value="5" />
+    <Square value="6" />
   </div>
   
   <div className="boardRow">
-    <Square />
-    <Square />
-    <Square />
+    <Square value="7" />
+    <Square value="8" />
+    <Square value="9" />
   </div>
   </>
   );
 }
 
-function Square()
+import {useState} from 'react';
+
+function Square({value})
 {
-  return <button className = "square">1</button>;
+  function buttonClick()
+  {
+    console.log('clicked!')
+  }
+  return <button
+    className = "square"
+    onClick={buttonClick}
+    >{value}</button>;
 }
